0b8e937dc1b4f11b559a1d340b7be6c70fa89bb3
sarinconp <sarinconp@unal.edu.co>
2019-09-06 08:49:15 -0500

    envio leer-2

---
+++ 2019-09-04-intro/leer-2.cpp
@@ -0,0 +1,13 @@
+
+
+#include<iostream>
+
+int main(void)
+{
+  char x =0.0;
+  std::cout <<  "Escribe una letra:\n";
+  std::cin >> x ;
+  std::cout << "\nTu letra es:\n"<< x<< "\n";
+
+    return 0;
+}
