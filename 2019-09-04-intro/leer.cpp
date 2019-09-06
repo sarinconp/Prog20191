609200607dff29116b4d0973c7d727ff4552821a
sarinconp <sarinconp@unal.edu.co>
2019-09-06 08:01:27 -0500

    subida ejercicios de clase

---
+++ 2019-09-04-intro/leer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void)
+{
+  double x = 0.0;
+  std::cout << "escribe un numero :\n" ;
+  std::cin >> x;
+  std::cout << "he leido : " << x << "\n";
+
+  return 0;
+
+}
